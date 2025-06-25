81b9264f627784f06ab3dd46e7f375f5b1226a5b
Jendelf <goncharukes04@gmail.com>
2025-06-25 16:33:46 +0500

    Initial project structure

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, HTTPException
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+
+async def root():
+    return {"message": "hello world"}
+
+if __name__ == "__main__":
+    uvicorn.run ("main:app", reload=True)
